4cfa0a8eaf7aef0868fc0e6a78cc460ef25fa89d
Karel Wintersky <karel.wintersky@yandex.ru>
2022-09-08 22:38:33 +0300

    0.8.6
    * + say function (echo message if in verbose mode)
    * fixed https://github.com/KarelWintersky/kwBackup/issues/4
    * + pv for piped database tasks
    * fixed https://github.com/KarelWintersky/kwBackup/issues/2
    * updated config example

--- kwbackup.sh
+++ kwbackup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.8.5"
+VERSION="0.8.6"
 
 THIS_SCRIPT="${0}"
 THIS_SCRIPT_BASEDIR="$(dirname ${THIS_SCRIPT})"
@@ -32,7 +32,7 @@ Options:
   -m, --sync-mode=copy|sync   Override upload mode [OPTIONAL]
   -f, --force                 Force backup section, overrides ENABLE_BACKUP_*
   --verbose                   More messages (mainly for debug)
-  --install                   Install prerequesites (RAR & PIGZ)
+  --install                   Install prerequisites (PV, RAR & PIGZ )
   -h, --help                  Print this help and exit
   -v, --version               Print version and exit
 "
@@ -54,7 +54,7 @@ function defineColors() {
 # а по окончанию работы файл стирать
 # в тот же файл писать временные логи процесса - чтобы было понятно, что происходит или в чем облом процесса
 function checkUniqueProcess() {
-    if pidof -o %PPID -x `basename "$0"` > /dev/null; then
+    if pidof -o %PPID -x $(basename "$0") >/dev/null; then
         echo "$(date "+%d.%m.%Y %T") EXIT: The script is already running." | tee -a "${LOG_FILE:-/var/log/kwbackup.log}"
         exit 1
     fi
@@ -64,8 +64,11 @@ function checkUniqueProcess() {
 function parseArgs {
     set -o errexit -o pipefail -o noclobber -o nounset
 
-    ! getopt --test > /dev/null
-    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then echo "I’m sorry, `getopt --test` failed in this environment."; exit 1; fi
+    ! getopt --test >/dev/null
+    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+        echo "I’m sorry, $(getopt --test) failed in this environment."
+        exit 1
+    fi
 
     local LONGOPTS=config:,database,storage,archive,help,sync-mode:,force,version,install,verbose
     local OPTIONS=c:hdsam:fv
@@ -78,69 +81,69 @@ function parseArgs {
 
     while true; do
         case "$1" in
-            -d|--database)
-                ACTION_BACKUP_DATABASE=y;
-                shift;
+        -d | --database)
+            ACTION_BACKUP_DATABASE=y
+            shift
             ;;
-            -s|--storage)
-                ACTION_BACKUP_STORAGE=y;
-                shift;
+        -s | --storage)
+            ACTION_BACKUP_STORAGE=y
+            shift
             ;;
-            -a|--archive)
-                ACTION_BACKUP_ARCHIVE=y;
-                shift;
+        -a | --archive)
+            ACTION_BACKUP_ARCHIVE=y
+            shift
             ;;
-            -h|--help)
-                ACTION_DISPLAY_HELP=y;
-                shift;
+        -h | --help)
+            ACTION_DISPLAY_HELP=y
+            shift
             ;;
-            -c|--config)
-                export CONFIG_FILE="$2";
-                shift 2;
+        -c | --config)
+            export CONFIG_FILE="$2"
+            shift 2
             ;;
-            -m|--sync-mode)
-                local RSM="$2";
-                case $RSM in
-                    "copy")
-                        CLI_UPLOAD_MODE="copy";
-                    ;;
-                    "sync")
-                        CLI_UPLOAD_MODE="sync";
-                    ;;
-                    #"sql")
-                    #    CLI_UPLOAD_MODE="sql";
-                    #;;
-                    *)
-                        echo "Invalid sync algorithm, must be 'copy' or 'sync'";
-                        exit 5;
-                    ;;
-                esac
-                shift 2;
-            ;;
-            --install)
-                curl ${RAR_DEB_URI} -o /tmp/rar.deb && sudo dpkg -i /tmp/rar.deb && rm /tmp/rar.deb
-                sudo apt install pigz
-                exit 0;
-            ;;
-            -f|--force)
-                ACTION_FORCE=y;
-                shift;
-            ;;
-            --verbose)
-                VERBOSE_MODE=y
-                shift;
-            ;;
-            -v|--version)
-                echo "${__what_is_it}"
-                exit 0;
-            ;;
-            --)
-                shift;
-                break;
-            ;;
+        -m | --sync-mode)
+            local RSM="$2"
+            case $RSM in
+            "copy")
+                CLI_UPLOAD_MODE="copy"
+                ;;
+            "sync")
+                CLI_UPLOAD_MODE="sync"
+                ;;
+            #"sql")
+            #    CLI_UPLOAD_MODE="sql";
+            #;;
             *)
-                echo "Programming error";
-                exit 3;
+                echo "Invalid sync algorithm, must be 'copy' or 'sync'"
+                exit 5
+                ;;
+            esac
+            shift 2
+            ;;
+        --install)
+            curl ${RAR_DEB_URI} -o /tmp/rar.deb && sudo dpkg -i /tmp/rar.deb && rm /tmp/rar.deb
+            sudo apt install pigz pv
+            exit 0
+            ;;
+        -f | --force)
+            ACTION_FORCE=y
+            shift
+            ;;
+        --verbose)
+            VERBOSE_MODE=y
+            shift
+            ;;
+        -v | --version)
+            echo "${__what_is_it}"
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
             ;;
         esac
     done
@@ -155,7 +158,14 @@ function displayHelp() {
 }
 
 function displayConfigError() {
-    echo -e "Config file ${CONFIG_FILE} ${ANSI_RED}not found${ANSI_RESET}";
+    echo -e "Config file ${CONFIG_FILE} ${ANSI_RED}not found${ANSI_RESET}"
+}
+
+function say() {
+    local MESSAGE=$1
+    if [[ "${VERBOSE_MODE}" = "y" ]]; then
+        echo -e ${MESSAGE}
+    fi
 }
 
 # суб-функция, которая делает бэкап на самом деле. Принимает 2 параметра:
@@ -165,44 +175,51 @@ function displayConfigError() {
 # несколько БД бэкапятся в CONTAINER/DB/(DAILY|WEEKLY|MONTHLY)/*
 function sub_backupDatabase() {
     local DB=$1
-    local CONTAINER_SUBPATH=$2
+    if [[ ${2} == "" ]]; then
+        PATH_INSIDE_CONTAINER=${CLOUD_CONTAINER_DB}
+    else
+        PATH_INSIDE_CONTAINER=${CLOUD_CONTAINER_DB}/${2}
+    fi
+    # ${PATH_INSIDE_CONTAINER}
 
-    echo "-----===== Backupping ${DB} =====-----"
+    # Для verbose mode делать не пайп
+    say "-----===== Backupping ${DB} =====-----"
     case "${USE_ARCHIVER}" in
-        "rar")
-            FILENAME_ARCHIVE=${DB}_${NOW}.sql.rar
-            mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" | rar a -si${DB}_${NOW}.sql ${RAR_OPTIONS} ${TEMP_PATH}/${FILENAME_ARCHIVE}
+    "rar")
+        FILENAME_ARCHIVE=${DB}_${NOW}.sql.rar
+        mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" | pv | rar a -si${DB}_${NOW}.sql ${RAR_OPTIONS} ${TEMP_PATH}/${FILENAME_ARCHIVE}
         ;;
-        "zip")
-            FILENAME_ARCHIVE=${DB}_${NOW}.sql.gz
-            mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" | pigz -c > ${TEMP_PATH}/${FILENAME_ARCHIVE}
+    "zip")
+        FILENAME_ARCHIVE=${DB}_${NOW}.sql.gz
+        mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" | pv | pigz -c >${TEMP_PATH}/${FILENAME_ARCHIVE}
         ;;
-        *)
-            FILENAME_ARCHIVE=${DB}_${NOW}.sql
-            mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" > ${TEMP_PATH}/${FILENAME_ARCHIVE}
+    *)
+        FILENAME_ARCHIVE=${DB}_${NOW}.sql
+        mysqldump ${MYSQL_OPTIONS} -h ${MYSQL_HOST} "${DB}" | pv > ${TEMP_PATH}/${FILENAME_ARCHIVE}
         ;;
     esac
 
     #@todo: сделать глубину хранения копий все таки зависимой от параметров, но со значением по умолчанию
+    # DB_MIN_AGE_DAILY, DB_MIN_AGE_WEEKLY, DB_MIN_AGE_MONTHLY (какая-то с этим была проблема)
 
     if [[ ${DB_BACKUP_DAILY:-0} = 1 ]]; then
-        rclone delete --config ${RCLONE_CONFIG} --min-age 7d ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/DAILY
-        rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/DAILY
+        rclone delete --config ${RCLONE_CONFIG} --min-age 7d ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/DAILY
+        rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/DAILY
     fi
 
     if [[ ${DB_BACKUP_WEEKLY:-0} = 1 ]]; then
         # if it is a sunday (7th day of week) - make store weekly backup (42 days = 7*6 + 1, so we storing last six weeks)
         if [[ ${NOW_DOW} -eq 1 ]]; then
-            rclone delete --config ${RCLONE_CONFIG} --min-age 43d ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/WEEKLY
-            rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/WEEKLY
+            rclone delete --config ${RCLONE_CONFIG} --min-age 43d ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/WEEKLY
+            rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/WEEKLY
         fi
     fi
 
     if [[ ${DB_BACKUP_MONTHLY:-0} = 1 ]]; then
-    # backup for first day of month
+        # backup for first day of month
         if [[ ${NOW_DAY} == 01 ]]; then
-            rclone delete --config ${RCLONE_CONFIG} --min-age 360d ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/MONTHLY
-            rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_DB}/${CONTAINER_SUBPATH}/MONTHLY
+            rclone delete --config ${RCLONE_CONFIG} --min-age 360d ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/MONTHLY
+            rclone copy --config ${RCLONE_CONFIG} ${RCLONE_OPTIONS} "${TEMP_PATH}"/"${FILENAME_ARCHIVE}" ${RCLONE_PROVIDER}:${PATH_INSIDE_CONTAINER}/MONTHLY
         fi
     fi
 
@@ -213,8 +230,8 @@ function sub_backupDatabase() {
 function actionBackupDatabase() {
     if [[ ${ENABLE_BACKUP_DATABASE:-0} = 0 ]]; then
         if [[ "${ACTION_FORCE:-n}" = "n" ]]; then
-            echo "Backup database disabled";
-            exit 0;
+            echo "Backup database disabled"
+            exit 0
         fi
     fi
 
@@ -222,24 +239,28 @@ function actionBackupDatabase() {
     FILENAME_ARCHIVE=
     MYSQL_OPTIONS="-Q --no-tablespaces --extended-insert=false --single-transaction"
 
+    # Опции брать из конфига.
     if [[ "${VERBOSE_MODE}" = "y" ]]; then
-        RAR_OPTIONS="-m5 -mde"
-        RCLONE_OPTIONS="--copy-links --update --verbose --progress"  # -LPuv
+        RAR_OPTIONS="-m3 -mdc"
+        RCLONE_OPTIONS="--copy-links --update --verbose --progress" # -LPuv
     else
-        RAR_OPTIONS="-m5 -mde -inul"
+        RAR_OPTIONS="-m3 -mdc -inul"
         RCLONE_OPTIONS="--copy-links --update"
     fi
 
     if [[ "$(declare -p DATABASES)" =~ "declare -a" ]]; then
-        # бэкапим несколько БД
-        for DB in "${DATABASES[@]}"
-        do
-            sub_backupDatabase ${DB} ${DB};
-        done
+        if [[ "${#DATABASES[@]}" == "1" ]]; then
+            local DB=${DATABASES[0]}
+            sub_backupDatabase ${DB} ""
+        else
+            for DB in "${DATABASES[@]}"; do
+                sub_backupDatabase ${DB} ${DB}
+            done
+        fi
     else
         # бэкапим одну БД
         local DB=${DATABASES}
-        sub_backupDatabase ${DB} "";
+        sub_backupDatabase ${DB} ""
     fi
 }
 
@@ -247,8 +268,8 @@ function actionBackupDatabase() {
 function actionBackupStorage() {
     if [[ ${ENABLE_BACKUP_STORAGE:-0} = 0 ]]; then
         if [[ "${ACTION_FORCE:-n}" = "n" ]]; then
-            echo "Backup storage disabled";
-            exit 0;
+            echo "Backup storage disabled"
+            exit 0
         fi
     fi
 
@@ -258,8 +279,7 @@ function actionBackupStorage() {
     local SOURCE_ROOT=${STORAGE_SOURCES_ROOT:-}
 
     if [[ "$(declare -p STORAGE_SOURCES)" =~ "declare -a" ]]; then
-        for SOURCE in "${STORAGE_SOURCES[@]}"
-        do
+        for SOURCE in "${STORAGE_SOURCES[@]}"; do
             rclone ${UPLOAD_MODE} --config ${RCLONE_CONFIG} -Luv --progress ${SOURCE_ROOT}${SOURCE} ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_STORAGE}/${SOURCE}
         done
     else
@@ -272,75 +292,74 @@ function actionBackupStorage() {
 function actionBackupArchive() {
     if [[ ${ENABLE_BACKUP_ARCHIVE:-0} = 0 ]]; then
         if [[ "${ACTION_FORCE:-n}" = "n" ]]; then
-            echo "Backup archive disabled";
-            exit 0;
+            echo "Backup archive disabled"
+            exit 0
         fi
     fi
 
     local UPLOAD_MODE=${CLI_UPLOAD_MODE:-${ARCHIVE_BACKUP_ALGO:-copy}}
 
-    rar a -x@${RARFILES_EXCLUDE_LIST} -m5 -mde -s -r ${TEMP_PATH}/${FILENAME_RAR} @${RARFILES_INCLUDE_LIST}
+    rar a -x@${RARFILES_EXCLUDE_LIST} -m3 -mdc -s -r ${TEMP_PATH}/${FILENAME_RAR} @${RARFILES_INCLUDE_LIST}
 
     rclone delete --config ${RCLONE_CONFIG} --min-age 71d ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_ARCHIVE}/
     rclone ${UPLOAD_MODE} --config ${RCLONE_CONFIG} -Luv ${TEMP_PATH}/${FILENAME_RAR} ${RCLONE_PROVIDER}:${CLOUD_CONTAINER_ARCHIVE}/
 
     rm ${TEMP_PATH}/${FILENAME_RAR}
 
-    exit;
+    exit
 }
 
-
 function main() {
-  defineColors;
-  checkUniqueProcess "$@";
-  parseArgs "$@";
+    defineColors
+    checkUniqueProcess "$@"
+    parseArgs "$@"
 
-  if [ ${ACTION_DISPLAY_HELP} = "y" ]; then
-      displayHelp;
-      exit 1;
-  fi
+    if [ ${ACTION_DISPLAY_HELP} = "y" ]; then
+        displayHelp
+        exit 1
+    fi
 
-  if [ ! -f "${CONFIG_FILE}" ]; then
-      displayConfigError;
-      exit 4;
-  fi
+    if [ ! -f "${CONFIG_FILE}" ]; then
+        displayConfigError
+        exit 4
+    fi
 
-  if [ "${VERBOSE_MODE}" = "y" ]; then
-      echo "${__what_is_it}";
-  fi
+    if [ "${VERBOSE_MODE}" = "y" ]; then
+        echo "${__what_is_it}"
+    fi
 
-  # Импортируем конфиг проекта
+    # Импортируем конфиг проекта
 
-  . ${CONFIG_FILE}
+    . ${CONFIG_FILE}
 
-  # проверяем существование глобального rclone.conf и говорим, что будем грузить его
-  if [ -f "${THIS_SCRIPT_BASEDIR}/rclone.conf" ]; then
-      RCLONE_CONFIG="${THIS_SCRIPT_BASEDIR}/rclone.conf"
-  fi
+    # проверяем существование глобального rclone.conf и говорим, что будем грузить его
+    if [ -f "${THIS_SCRIPT_BASEDIR}/rclone.conf" ]; then
+        RCLONE_CONFIG="${THIS_SCRIPT_BASEDIR}/rclone.conf"
+    fi
 
-  local NO_ACTION=1
+    local NO_ACTION=1
 
-  if [ ${ACTION_BACKUP_DATABASE} = "y" ]; then
-      actionBackupDatabase;
-      NO_ACTION=0;
-  fi
+    if [ ${ACTION_BACKUP_DATABASE} = "y" ]; then
+        actionBackupDatabase
+        NO_ACTION=0
+    fi
 
-  if [ ${ACTION_BACKUP_STORAGE} = "y" ]; then
-      actionBackupStorage;
-      NO_ACTION=0;
-  fi
+    if [ ${ACTION_BACKUP_STORAGE} = "y" ]; then
+        actionBackupStorage
+        NO_ACTION=0
+    fi
 
-  if [ ${ACTION_BACKUP_ARCHIVE} = "y" ]; then
-      actionBackupArchive;
-      NO_ACTION=0;
-  fi
+    if [ ${ACTION_BACKUP_ARCHIVE} = "y" ]; then
+        actionBackupArchive
+        NO_ACTION=0
+    fi
 
-  if [ ${NO_ACTION=0;} = 1 ]; then
-    echo -e "No one backup action(s) requested. Allowed: "
-    if [ ${ENABLE_BACKUP_DATABASE} = 1 ]; then echo -e "... database (use ${ANSI_YELLOW}--database${ANSI_RESET} option)"; fi
-    if [ ${ENABLE_BACKUP_STORAGE} = 1 ]; then echo -e  "... storage  (use ${ANSI_YELLOW}--storage${ANSI_RESET}  option)"; fi
-    if [ ${ENABLE_BACKUP_ARCHIVE} = 1 ]; then echo -e  "... archive  (use ${ANSI_YELLOW}--archive${ANSI_RESET}  option)"; fi
-  fi
+    if [ ${NO_ACTION=0;} = 1 ]; then
+        echo -e "No one backup action(s) requested. Allowed: "
+        if [ ${ENABLE_BACKUP_DATABASE} = 1 ]; then echo -e "... database (use ${ANSI_YELLOW}--database${ANSI_RESET} option)"; fi
+        if [ ${ENABLE_BACKUP_STORAGE} = 1 ]; then echo -e "... storage  (use ${ANSI_YELLOW}--storage${ANSI_RESET}  option)"; fi
+        if [ ${ENABLE_BACKUP_ARCHIVE} = 1 ]; then echo -e "... archive  (use ${ANSI_YELLOW}--archive${ANSI_RESET}  option)"; fi
+    fi
 }
 
 # ---------------- Main ----------------
